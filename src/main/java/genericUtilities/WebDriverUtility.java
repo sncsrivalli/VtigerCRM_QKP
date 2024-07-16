3c7ef68ccd468d773e9e3c7126b66fd3ec87a6dd
Srivalli <snc.srivalli1@gmail.com>
2024-07-16 10:56:03 +0530

    Added WebDriverUtility class

---
+++ src/main/java/genericUtilities/WebDriverUtility.java
@@ -0,0 +1,250 @@
+package genericUtilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * This class contains reusable methods to perform driver related operations
+ * 
+ * @author sncsr
+ *
+ */
+public class WebDriverUtility {
+
+	WebDriver driver;
+	WebDriverWait wait;
+	Actions actions;
+	Select select;
+
+	/**
+	 * This method launches the user desired browser
+	 * 
+	 * @param browser
+	 * @return WebDriver
+	 */
+	public WebDriver launchBrowser(String browser) {
+		if (browser.equalsIgnoreCase("chrome"))
+			driver = new ChromeDriver();
+		else if (browser.equalsIgnoreCase("firefox"))
+			driver = new FirefoxDriver();
+		else if (browser.equalsIgnoreCase("edge"))
+			driver = new EdgeDriver();
+		else
+			System.out.println("Invalid browser");
+
+		return driver;
+	}
+
+	/**
+	 * This method maximizes the browser window
+	 */
+	public void maximizeBrowser() {
+		driver.manage().window().maximize();
+	}
+
+	/**
+	 * This method navigates to an application
+	 * 
+	 * @param url
+	 */
+	public void navigateToApp(String url) {
+		driver.get(url);
+	}
+
+	/**
+	 * This method waits till element or elements are found
+	 * 
+	 * @param time
+	 */
+	public void waitTillElementFound(long time) {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(time));
+	}
+
+	/**
+	 * This method waits till element is displayed on the web page
+	 * 
+	 * @param time
+	 * @param element
+	 * @return WebElement
+	 */
+	public WebElement explicitWait(long time, WebElement element) {
+		wait = new WebDriverWait(driver, Duration.ofSeconds(time));
+		return wait.until(ExpectedConditions.visibilityOf(element));
+	}
+
+	/**
+	 * This method waits till the element is enabled to receive click
+	 * 
+	 * @param element
+	 * @param time
+	 * @return WebElement
+	 */
+	public WebElement explicitWait(WebElement element, long time) {
+		wait = new WebDriverWait(driver, Duration.ofSeconds(time));
+		return wait.until(ExpectedConditions.elementToBeClickable(element));
+	}
+
+	/**
+	 * This method waits till title of the web page appears
+	 * 
+	 * @param time
+	 * @param title
+	 * @return Boolean
+	 */
+	public Boolean explicitWait(long time, String title) {
+		wait = new WebDriverWait(driver, Duration.ofSeconds(time));
+		return wait.until(ExpectedConditions.titleContains(title));
+	}
+
+	/**
+	 * This method is used to mouse hover on an element
+	 * 
+	 * @param element
+	 */
+	public void mouseHover(WebElement element) {
+		actions = new Actions(driver);
+		actions.moveToElement(element).perform();
+	}
+
+	/**
+	 * This method is used to perform double click on an element
+	 * 
+	 * @param element
+	 */
+	public void doubleClickOnElement(WebElement element) {
+		actions = new Actions(driver);
+		actions.doubleClick(element).perform();
+	}
+
+	/**
+	 * This method is used to right click on an element
+	 * 
+	 * @param element
+	 */
+	public void rightClick(WebElement element) {
+		actions = new Actions(driver);
+		actions.contextClick(element).perform();
+	}
+
+	/**
+	 * This method is used to drag and drop an element to destination
+	 * 
+	 * @param element
+	 * @param dest
+	 */
+	public void dragAndDropAnElement(WebElement element, WebElement dest) {
+		actions = new Actions(driver);
+		actions.dragAndDrop(element, dest).perform();
+	}
+
+	/**
+	 * This method selects an option from drop down based on index
+	 * 
+	 * @param element
+	 * @param index
+	 */
+	public void handleDropdown(WebElement element, int index) {
+		select = new Select(element);
+		select.selectByIndex(index);
+	}
+
+	/**
+	 * This method selects an option from drop down based on value attribute
+	 * 
+	 * @param element
+	 * @param value
+	 */
+	public void handleDropdown(WebElement element, String value) {
+		select = new Select(element);
+		select.selectByValue(value);
+	}
+
+	/**
+	 * This method selects an option from drop down based on visible text
+	 * 
+	 * @param text
+	 * @param element
+	 */
+	public void handleDropdown(String text, WebElement element) {
+		select = new Select(element);
+		select.selectByVisibleText(text);
+	}
+
+	/**
+	 * This method is used to switch to frame based on frame index
+	 * 
+	 * @param index
+	 */
+	public void switchToFrame(int index) {
+		driver.switchTo().frame(index);
+	}
+
+	/**
+	 * This method is used to switch to frame based on id or name attribute value
+	 * 
+	 * @param idOrNameAttribute
+	 */
+	public void switchToFrame(String idOrNameAttribute) {
+		driver.switchTo().frame(idOrNameAttribute);
+	}
+
+	/**
+	 * This method is used to switch to frame based on frame element
+	 * 
+	 * @param frameElement
+	 */
+	public void switchToFrame(WebElement frameElement) {
+		driver.switchTo().frame(frameElement);
+	}
+
+	/**
+	 * This method is used to switch to back from frame
+	 */
+	public void switchBackFromFrame() {
+		driver.switchTo().defaultContent();
+	}
+
+	/**
+	 * This method captures the screenshot of a web page
+	 * @param driver
+	 * @return String
+	 */
+	public String getScreenshot(WebDriver driver) {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		return ts.getScreenshotAs(OutputType.BASE64);
+	}
+
+	/**
+	 * This method captures the screenshot of a web page
+	 * @param driver
+	 * @param className
+	 * @param jutil
+	 * @return
+	 */
+	public String getScreenshot(WebDriver driver, String className, JavaUtility jutil) {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File temp = ts.getScreenshotAs(OutputType.FILE);
+		File dest = new File("./Screenshots/" + className + "_" + jutil.getCurrentTime() 
+																				+ ".png");
+		try {
+			FileUtils.copyFile(temp, dest);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return dest.getAbsolutePath();
+	}
+}

e29fd521983e437703dc244fea382711f41df9cc
jagua <jagua@DESKTOP-IP4K28O>
2024-08-13 09:34:45 +0530

    change done by user1

--- src/test/java/testNGImplementation/CreateOrganizationTest.java
+++ src/test/java/testNGImplementation/CreateOrganizationTest.java
@@ -9,7 +9,7 @@ import genericUtilities.TabNames;
 import objectRepo.CreatingNewOrganizationPage;
 import objectRepo.OrganizationInformationPage;
 import objectRepo.OrganizationsPage;
-
+// This test validates if user is able to create new Organization
 public class CreateOrganizationTest extends BaseClass {
 
 	@Test(groups = "organizations")
